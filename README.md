af4b91036433d351bb47831cab7f79059987d462
Edward Lim <ed.lim199@gmail.com>
2024-11-19 11:24:14 +1100

    instructions and dependencies

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Given a wallet address, the program outputs a csv detailing the token transfers associated with it.
+
+# Instructions
+```
+python3 -m pip install -r requirements.txt
+python3 main.py
+```
--- main.py
+++ main.py
@@ -15,14 +15,29 @@ def main():
     }
     response = requests.get(f"https://polygonscan.com/address/{wallet_address}#tokentxns", headers=headers, timeout=10)
 
-    print(response)
     if response.status_code != 200:
         print("Invalid Wallet Address", sys.stderr)
         return 1
 
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    print(soup.prettify())
+    rows = soup.find_all('tr')
+
+    print("transaction hash | age | from | in/out | to | amount | token")
+    for row in rows:
+        # second td is transaction hash
+        cells = row.find_all('td')
+        print(len(cells))
+        # transaction_hash = cells[1].find('a')['href']
+        # print(transaction_hash)
+        # age = cells[4]
+        # sender = cells[6]
+        # in_out = cells[7]
+        # to = cells[8]
+        # amount = cells[9]
+        # token = cells[10]
+        # p
+
 
 if __name__ == "__main__":
     main()
